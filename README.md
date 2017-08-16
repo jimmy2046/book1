697c44215a339fc528c6b6923f8b13a8ff57a4d3
jimmy2046 <jimmychong_st@yahoo.com>
2017-08-15 17:50:02 -0700

    Finished Chapter 6

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # README
 * HTML Codes from: http://www.htmlandcssbook.com/
+
+## Ruby on Rails Heroku hosting
+* https://lit-ravine-90777.herokuapp.com/
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,14 @@ Rails.application.routes.draw do
   get  'static_pages/ch05_05_aligning_images_vertically'    
   get  'static_pages/ch05_06_examining_images_on_the_web'
   get  'static_pages/ch05_07_figure-and-figure-caption'
+
+  get  'static_pages/ch06_01_basic_table_structure'    
+  get  'static_pages/ch06_02_table_headings'    
+  get  'static_pages/ch06_03_spanning_columns'
+  get  'static_pages/ch06_04_spanning_rows'
+  get  'static_pages/ch06_05_long_tables'
+  get  'static_pages/ch06_06_width_and_spacing'
+  get  'static_pages/ch06_07_border_and_background'
+  get  'static_pages/ch06_08_example'
     
 end
